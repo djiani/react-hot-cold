d9f8be5204f188575521dbd41e52600466f00e2f
Samuel R. Djiani <djiasara87@yahoo.fr>
2018-01-18 03:18:00 -0500

    react-redux drills

---
+++ src/actions.js
@@ -0,0 +1,18 @@
+export const RESTARTGAME = 'RESTARTGAME';
+export const restartGame = (correctAnswer) =>({
+  type: RESTARTGAME,
+  correctAnswer
+});
+
+export const MAKEGUESS = 'MAKEGUESS';
+export const makeguess = (guess)=>({
+  type: MAKEGUESS,
+  guess
+});
+
+export const GENERATEAURALUPDATE = 'GENERATEAURALUPDATE';
+export const generate_aural_update = () =>({
+  type: GENERATEAURALUPDATE
+})
+
+//valided, setGuessValue
---
+++ src/reducers.js
@@ -0,0 +1,63 @@
+import * as actions from './actions';
+
+const initialState = { 
+  
+  feedback: 'Make your guess!',
+  auralStatus: '',
+  correctAnswer: Math.round(Math.random() * 100) + 1,
+  guesses: []
+}
+
+export const hotColdReducer = (state=initialState, action)=> {
+  if (action.type === actions.RESTARTGAME){
+    return Object.assign({}, state, {
+      guesses: [],
+      feedback: 'Make your guess!',
+      auralStatus: '',
+      correctAnswer: Math.floor(Math.random() * 100) + 1
+    })
+  }
+  else if(action.type === actions.MAKEGUESS){
+    let guess = parseInt(action.action, 10);
+    let feedback = '';
+    if(isNaN(guess)){
+      feedback = 'Please enter a valid number';
+    }
+    else{
+      let difference = Math.abs(guess - this.state.correctAnswer);
+      if(difference > 50){
+        feedback = 'You\'re Ice Cold...';
+      } else if (difference >= 30) {
+        feedback = 'You\'re Cold...';
+      } else if (difference >= 10) {
+        feedback = 'You\'re Warm.';
+      } else if (difference >= 1) {
+        feedback = 'You\'re Hot!';
+      } else {
+        feedback = 'You got it!';
+      }
+    }
+    return Object.assign({}, state, {
+      feedback,
+      guesses: [...state.guess, guess]});
+    
+  }
+  else if(action.type === actions.GENERATEAURALUPDATE){
+    const { guesses, feedback } = state;
+
+    // If there's not exactly 1 guess, we want to
+    // pluralize the nouns in this aural update.
+    const pluralize = guesses.length !== 1;
+
+    let  auralStatus = `Here's the status of the game right now: ${feedback} You've made ${guesses.length} ${pluralize ? 'guesses' : 'guess'}.`;
+
+    if (guesses.length > 0) {
+      auralStatus += ` ${pluralize ? 'In order of most- to least-recent, they are' : 'It was'}: ${guesses.reverse().join(', ')}`;
+    }
+
+    return Object.assign({}, state, {
+      auralStatus
+    })
+  }
+}
+
